54229c31a27eaf959b376cbbee0cd607496148ef
Denys Stuienko <stuenkden@gmail.com>
2025-07-21 09:15:37 +0300

    added scss

---
+++ src/components/SignIn/SignInForm.tsx
@@ -0,0 +1,38 @@
+import { Container, Box, Typography, InputLabel, Input, Button, useTheme } from "@mui/material"
+import DownloadIcon from '@mui/icons-material/Download';
+import { AuthFormControl } from "../AuthFormControl/AuthFormControl"
+import './styles.scss';
+
+export const SignInForm = () => {
+    const theme = useTheme();
+
+    return <Container 
+        maxWidth={false} 
+        className='auth-layout__container' //using className for styling
+        //using sx prop for styling
+        // sx={{display: 'flex', justifyContent: 'center', alignItems: 'center' , height: '100vh'}} 
+        >
+        <Box
+        component={'form'}
+        className='auth-layout__form' //using className for styling
+        // using sx prop for styling
+        // sx={{'display': "flex", flexDirection: "column", alignItems: "center", gap: "10px", padding: "60px 96px", backgroundColor: theme.palette.customGrey[95], border: `1px solid ${theme.palette.customGrey[80]}`, width: '552px'}}
+        >
+        <Typography variant='h1'>Вхід</Typography>
+        <AuthFormControl sx={{width: '100%'}}>
+            <InputLabel htmlFor="email-input">Email</InputLabel>
+            <Input placeholder='Ваш email' id='email-input'/>
+        </AuthFormControl>
+        <AuthFormControl>
+            <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+            <InputLabel htmlFor="password-input">Пароль</InputLabel>
+            <Typography variant='body1'>Забули пароль?</Typography>
+            </Box>
+            <Input placeholder='Ваш пароль' id='password-input'/>
+        </AuthFormControl>
+        <Button variant='primary' size='large'>Далі</Button>
+        <Button variant='primary' size='medium'>Далі</Button>
+        <Button variant='primary' size='medium' endIcon={<DownloadIcon />}>Перейти до курсу</Button>
+        </Box>
+    </Container>
+}
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,17 +1,26 @@
-import { createTheme } from '@mui/material/styles';
 import palette from './palette';
 import typography from './typography';
 import { spacing, breakpoints, shadows, transitions } from './styles';
 import components from './components';
+import { extendTheme } from '@mui/material/styles';
 
-const theme = createTheme({
-  palette: palette,  
-  typography: typography, 
-  spacing: spacing, 
-  breakpoints: breakpoints,
-  shadows: shadows,
-  components: components,
-  transitions: transitions
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: palette,
+    },
+    dark: {
+      palette: {
+        ...palette,
+        mode: 'dark',
+      },
+    },
+  },
+  typography,
+  spacing,
+  breakpoints,
+  shadows,
+  components,
+  transitions,
 });
-
 export default theme;
